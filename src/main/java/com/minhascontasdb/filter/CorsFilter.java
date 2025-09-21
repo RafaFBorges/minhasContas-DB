6379cb00c3ff8d9741900fea56d92972e677cc25
Rafael <rafaeelfernandes@hotmail.com>
2025-09-21 12:13:10 -0300

    [bugfix] enable filter

--- src/main/java/com/minhascontasdb/filter/CorsFilter.java
+++ src/main/java/com/minhascontasdb/filter/CorsFilter.java
@@ -7,32 +7,31 @@ import jakarta.servlet.FilterConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletResponse;
 
+@WebFilter("/*")
 public class CorsFilter implements Filter {
 
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-        // Inicialização do filtro (opcional)
-    }
+  @Override
+  public void init(FilterConfig filterConfig) throws ServletException {
+  }
 
-    @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-            throws IOException, ServletException {
-        
-        HttpServletResponse httpResponse = (HttpServletResponse) response;
-        
-        // Adiciona os cabeçalhos de CORS
-        httpResponse.setHeader("Access-Control-Allow-Origin", "*"); // Permite qualquer origem
-        httpResponse.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS"); // Métodos permitidos
-        httpResponse.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization"); // Cabeçalhos permitidos
-        
-        // Continua o processamento da requisição
-        chain.doFilter(request, response);
-    }
+  @Override
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+      throws IOException, ServletException {
 
-    @Override
-    public void destroy() {
-        // Limpeza do filtro (opcional)
-    }
+    HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+    // Adiciona os cabeçalhos de CORS
+    httpResponse.setHeader("Access-Control-Allow-Origin", "*"); // Permite qualquer origem
+    httpResponse.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS"); // Métodos permitidos
+    httpResponse.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization"); // Cabeçalhos permitidos
+
+    chain.doFilter(request, response);
+  }
+
+  @Override
+  public void destroy() {
+  }
 }
