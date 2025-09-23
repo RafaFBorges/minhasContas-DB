8d4077ff33d88a07e92f689e05925c95f458ae75
Rafael Fernandes Borges <46011419+RafaFBorges@users.noreply.github.com>
2025-09-22 22:34:22 -0300

    [bugfix] Reconfig CORS filter #Serv 0 (#12)
    * [feature] allow CORS #SERV-0
    
    * [bugfix] enable filter
    
    * [bugfix] reconfig CORS filter #SERV-0

--- src/main/java/com/minhascontasdb/filter/CorsFilter.java
+++ src/main/java/com/minhascontasdb/filter/CorsFilter.java
@@ -1,37 +1,22 @@
 package com.minhascontasdb.filter;
 
-import java.io.IOException;
-import jakarta.servlet.Filter;
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.FilterConfig;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
-import jakarta.servlet.annotation.WebFilter;
-import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@WebFilter("/*")
-public class CorsFilter implements Filter {
-
-  @Override
-  public void init(FilterConfig filterConfig) throws ServletException {
-  }
-
-  @Override
-  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-      throws IOException, ServletException {
-
-    HttpServletResponse httpResponse = (HttpServletResponse) response;
-
-    // Adiciona os cabeçalhos de CORS
-    httpResponse.setHeader("Access-Control-Allow-Origin", "*"); // Permite qualquer origem
-    httpResponse.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS"); // Métodos permitidos
-    httpResponse.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization"); // Cabeçalhos permitidos
-
-    chain.doFilter(request, response);
-  }
-
-  @Override
-  public void destroy() {
+@Configuration
+public class CorsFilter {
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*");
+      }
+    };
   }
 }
