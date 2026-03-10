ae62e93c89ecef0fba7253751cc14db237e1e455
Rafael <rafaeelfernandes@hotmail.com>
2026-03-09 23:46:10 -0300

    [feature] send expireTime  #SERV-2

--- src/main/java/com/minhascontasdb/dto/LoginResponseDTO.java
+++ src/main/java/com/minhascontasdb/dto/LoginResponseDTO.java
@@ -1,14 +1,22 @@
 package com.minhascontasdb.dto;
 
+import java.time.LocalDateTime;
+
 public class LoginResponseDTO {
 
   private String token;
+  private LocalDateTime expireTime;
 
   public LoginResponseDTO(String token) {
     this.token = token;
+    this.expireTime = LocalDateTime.now().plusHours(1);
   }
 
   public String getToken() {
     return token;
+  }
+
+  public LocalDateTime getExpireTime() {
+    return expireTime;
   }
 }
