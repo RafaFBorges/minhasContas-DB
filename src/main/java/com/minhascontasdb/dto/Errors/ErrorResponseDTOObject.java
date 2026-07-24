d0a3931c115f429ddaa97411f67240dc05099de6
Rafael <rafaeelfernandes@hotmail.com>
2026-07-24 14:33:22 -0300

    [refactor] clean ErrorResponseDTOObject structure #SERV-2

--- src/main/java/com/minhascontasdb/dto/Errors/ErrorResponseDTOObject.java
+++ src/main/java/com/minhascontasdb/dto/Errors/ErrorResponseDTOObject.java
@@ -1,21 +1,11 @@
 package com.minhascontasdb.dto.Errors;
 
 public class ErrorResponseDTOObject extends RuntimeException {
-  private String message;
-
   public ErrorResponseDTOObject(String message) {
-    this.message = message;
-  }
-
-  public String getMessage() {
-    return message;
-  }
-
-  public void setMessage(String message) {
-    this.message = message;
+    super(message);
   }
 
   public ErrorResponseDTO getResponse() {
-    return new ErrorResponseDTO(this.message);
+    return new ErrorResponseDTO(this.getMessage());
   }
 }
